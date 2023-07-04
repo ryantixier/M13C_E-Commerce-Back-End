b4a9034d54380680e50aebc82148fd352a14785e
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-07-03 18:37:03 -0600

    Finished models.

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,24 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    category_name: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,24 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    tag_name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
